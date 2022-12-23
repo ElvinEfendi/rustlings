4a1c4a7fc6ec2faf6bec2fb9b5809978169c20f8
Elvin Efendiev <elvin.efendiev@shopify.com>
2022-12-23 17:02:15 -0500

    Some progress

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
-// Put your function here!
-// fn calculate_price_of_apples {
+pub fn calculate_price_of_apples(num: i32) -> i32 {
+    let mut cost: i32 = 2;
+    if num > 40 {
+        cost = 1;
+    }
+    num * cost
+}
 
 // Don't modify this function!
 #[test]
